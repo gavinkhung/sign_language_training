ac665a0355d35a1987121a77c698c48e126f7ac1
Gavin Hung <32819629+gavinkhung@users.noreply.github.com>
2019-10-14 15:12:31 -0700

    Add files via upload

--- load_mobile_net_numbers.py
+++ load_mobile_net_numbers.py
@@ -8,7 +8,7 @@ keras_file = "sign_numbers_model.h5"
 loaded_model_mines = tf.keras.models.load_model(keras_file)
 loaded_model = tf.keras.applications.mobilenet.MobileNet()
 
-img_path = "photos/0/IMG_4129.JPG"
+img_path = "photos/9/IMG_4128.JPG"
 
 print(img_path)
 
@@ -21,7 +21,7 @@ prediction_mines = loaded_model_mines.predict(pImg, batch_size=1000)[0].tolist()
 print("prediction results: ")
 labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 for i in range(len(prediction_mines)):
-    print(labels[i], prediction_mines[i])
+    print(labels[i],"\t",prediction_mines[i])
 
 total = 0
 for num in prediction_mines:
--- mobile_net_numbers.py
+++ mobile_net_numbers.py
@@ -16,32 +16,14 @@ valid_batches = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_fu
 test_batches = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(test_path, target_size=(224, 224), batch_size=10, shuffle=False)
 
 """
-mobile = tf.keras.applications.mobilenet.MobileNet()
-mobile.summary()
-
-x = mobile.layers[-6].output
-predictions = tf.keras.layers.Dense(10, activation='softmax')(x)
-model = tf.keras.models.Model(inputs=mobile.input, outputs=predictions)
-model.summary()
-
-for layer in model.layers[:-23]:
-    layer.trainable = False
-
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(train_batches, steps_per_epoch=18, validation_data=valid_batches, validation_steps=3, epochs=5, verbose=2)
-
-keras_file = "sign_numbers_model.h5"
-model.save(keras_file)
-"""
 base_model=tf.keras.applications.mobilenet.MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
-
 x=base_model.output
 x=tf.keras.layers.GlobalAveragePooling2D()(x)
 x=tf.keras.layers.Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
 x=tf.keras.layers.Dense(1024,activation='relu')(x) #dense layer 2
 x=tf.keras.layers.Dense(512,activation='relu')(x) #dense layer 3
 preds=tf.keras.layers.Dense(2,activation='softmax')(x) #final layer with softmax activation
-
+"""
 
 mobile = tf.keras.applications.mobilenet.MobileNet()
 mobile.summary()
@@ -55,7 +37,7 @@ for layer in model.layers[:-23]:
     layer.trainable = False
 
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(train_batches, steps_per_epoch=18, validation_data=valid_batches, validation_steps=3, epochs=30, verbose=2)
+model.fit_generator(train_batches, steps_per_epoch=18, validation_data=valid_batches, validation_steps=3, epochs=60, verbose=2)
 
 keras_file = "sign_numbers_model.h5"
 model.save(keras_file)
